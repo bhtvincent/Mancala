a779c6863b11aef77498f037e6d922a3d11b8ed9
Grant <33609420+clegggrant@users.noreply.github.com>
2017-11-12 17:02:12 -0800

    Created Model of Mancala Class. Contains functional Attach and Update methods; however, gameplay functionality has not been added.

---
+++ src/ModelOfMancala.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import javax.swing.event.*;
+public class ModelOfMancala 
+{
+	private ArrayList<Pit> pits;
+	private ArrayList<ChangeListener> listeners;
+	
+	/**
+	 * Creates a ModelOfMancala with empty pit and listener arraylists
+	 */
+	public ModelOfMancala()
+	{
+		pits = new ArrayList<>();
+		listeners = new ArrayList<>();
+	}
+	
+	/**
+	 * Attaches a ChangeListener to this model
+	 * @param cl the ChangeListener to attach to this model
+	 */
+	public void attach(ChangeListener cl)
+	{
+		listeners.add(cl);
+	}
+	
+	/**
+	 * Updates the ChangeListeners of this Model
+	 * if there is a change
+	 */
+	public void update()
+	{
+		for(ChangeListener cl : listeners)
+		{
+			cl.stateChanged(new ChangeEvent(this));
+		}
+	}
+	
+	/**
+	 * Moves the Marbles from one pit into the next
+	 * pits. Not yet Functional
+	 */
+	public void moveMade()
+	{
+		return;
+	}
+}
