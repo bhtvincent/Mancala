41aa887133336443284268d32953d2f39f4523a0
bhtvincent <bhtbi@yahoo.com>
2017-12-03 19:39:12 -0800

    Created Mancala Pit
    disregard all the main methods everywhere lmao

--- src/MancalaPit.java
+++ src/MancalaPit.java
@@ -1,10 +1,30 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MancalaPit extends Pit
 {
-	public MancalaPit()
+	public MancalaPit(int numOfMarbles, int x, int y)
 	{
-		super();
-		this.setText("0");
+		super(numOfMarbles,x,y);
 		this.setEnabled(false);
+		this.setPreferredSize((new Dimension(100,250)));
+	}
+	
+
+	public static void main(String[] args)
+	{
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.setSize(200, 200);
+		MancalaPit pit = new MancalaPit(0,10,10);
+		panel.add(pit);	
+		frame.add(panel);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 }
