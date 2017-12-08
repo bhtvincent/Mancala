069b04bb8cef98ff78e0a7f53a6365a780151526
FernBurn <dfonyo@yahoo.com>
2017-12-07 20:53:11 -0800

    added javadoc

--- src/StrategyOne.java
+++ src/StrategyOne.java
@@ -1,5 +1,14 @@
 import java.awt.Color;
 import java.util.ArrayList;
+
+/**
+ * First concrete strategy that implements the StyleStrategy interface and acts as a selectable style for the user.
+ * @author Grant Clegg
+ * @author Daniel Fonyo
+ * @author Vincent Tran
+ * @version December 7, 2017
+ */
+
 public class StrategyOne implements StyleStrategy
 {	
 	public void createBoard(ArrayList<Pit> pits, ArrayList<MancalaPit> mancalas)
